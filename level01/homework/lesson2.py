eae25aae32614931e392b52c7e575c373bc3d9e5
Matkava25 <matkava2577@gmail.com>
2024-06-23 13:49:31 +0400

    commit

--- level01/homework/lesson2.py
+++ level01/homework/lesson2.py
@@ -1,33 +1,6 @@
 name = "levan"
-#name არის ცვლადი
-# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
-# "levan" არის ცვლადისთვის მნიშვნელობა
+lastname = "matkava"
+age = 25 
+height = 187
 
-surname = "matkava"
-
-#print(name)
-#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
-
-name = "levan" #ეს არის str (string) ტიპის ცვლადი
-age = 25 # ეს არის int (integer) მთელი რიცხვი
-height = 187 #ეს არის float ტიპის ცვლადი (ათწილადი)
-#boolean (bool) ტიპის ცვლადი
-
-knows_programming = True #True ან False
-is_ugly = False #snakecase(უბრალოდ წერის სტილი სტანდარტულად)
-
-isUgly = False #ჯავასკრიპტული camelcase
-
-
-print(name + "" + surname)
-
-# #სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
-# print(name+age)
-
-print (type(age))
-print (type(name))
-print (type(surname))
-print (type(height))
-print (type(knows_programming))
-
-print(name+" " +str(age))
+print ("me mqvia", name , lastname , "wlovanebit var" , str(age) , "da simaglit var" , height)
