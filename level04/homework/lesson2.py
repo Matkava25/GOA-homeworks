8d24302128cf641a2efab901a5b57858df7cacac
Matkava25 <matkava2577@gmail.com>
2024-07-06 16:32:57 +0400

    commit

---
+++ level04/homework/lesson2.py
@@ -0,0 +1,6 @@
+name = input("enter your name: ")
+lastname = input("enter your lastname: ")
+age = input("enter your age: ")
+city =input("enter your city: ")
+
+print("es adamiani aris" + " " + name + " " + lastname + " " + "gaxda" + " " + age + " " + "wlis, da gadavida sacxovreblad qalaq" + " " + city )
---
+++ level04/homework/lesson3.py
@@ -0,0 +1,7 @@
+# input output
+
+# input - შეტანა / შეყვანა
+# output - გამოტანა / გამოყვანა
+
+name = input("enter your name: ")
+print("hello " + name)
