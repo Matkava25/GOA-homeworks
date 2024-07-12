26c48a40b416d9347b5653ac14aa8293e0dda788
Matkava25 <matkava2577@gmail.com>
2024-07-12 13:19:45 +0400

    commit

---
+++ level05/homework/lesson1.py
@@ -0,0 +1,52 @@
+# 1) გააკეთეთ 3 ცალი სწორი კოდი და 3 ცალი არასწორი კოდი და უნდა ახსნათ რატომ არის ეს კოდი სწორი და რატომ არის ეს კოდი არასწორი
+# 1 სწორი
+
+name = "We have 5 ocean in the world: "
+
+ocean = "pacific, "
+ocean2 = "atlantic, "
+ocean3 = "indian, "
+ocean4 = "arctic, "
+ocean5 = "southern. "
+print(name + ocean + ocean2 + ocean3 + ocean4 + ocean5)
+
+# 1 არასწორი
+
+#name_"We have 5 ocean in the world"
+
+#ocean-"pacific,"
+#ocean2_"atlantic"
+#ocean3-"indian"
+#ocean4-"arctic::"
+#ocean5-"southern.."
+#print"name+ocean+ocean2+ocean3+ocean4+ocean5"""
+
+# 2 სწორი
+
+name = "people play: "
+
+game = "dota2, "
+game2 = "valorant, "
+game3 = "cs2, "
+game4 = "tank. "
+print(name + game + game2 + game3 + game4)
+
+# 2 არასწორი
+
+#name_:people play:
+
+#game_:dota2:
+#game2 - valorant"
+#game3 - cs2:
+#game4 + "tank"
+#print"name+dota2_+valorant+cs2_tank
+
+# 3 სწორი
+
+name = "4 teams reached the quarter finals: "
+
+team = "spain, "
+team2 = "germany, "
+team3 = "nederlands, "
+team4 = "england. "
+print(name + team + team2 + team3 + team4)
